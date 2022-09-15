fc40edb57c3ee71c9adb43cc2dede8d62df85c19
Viktor De Pasquale <info@depasquale.wiki>
2022-09-15 10:06:09 +0200

    Removed implicit usage of dash view

--- src/ui/router/index.ts
+++ src/ui/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import DashView from "../views/DashView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +6,7 @@ const router = createRouter({
     {
       path: "/",
       name: "dash",
-      component: DashView,
+      component: () => import("@/ui/views/DashView.vue"),
     },
     {
       path: "/requests",
